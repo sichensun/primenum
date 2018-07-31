84c2f877fd0077adc17a3a5f82f3427a6ef9bbcd
sichen <sichen_sun@outlook.com>
2018-07-31 21:48:29 +0800

    add note in primenum.c

--- primenum.c
+++ primenum.c
@@ -2,6 +2,18 @@
 #include<math.h>
 #include<stdlib.h>
 
+//Author:sichen
+
+/********************
+
+  This program determines whether a number is a prime number.
+  Divided a number by a list from 2 to sqrt(n),not from 2 to n,to save running time.
+
+  Using unsigned long instead of int,to show a larger number;
+  My runtime environment is archlinux x64,the unsigned long on my system is 64 bit.
+
+********************/
+
 int main()
 {
 	unsigned long number;
